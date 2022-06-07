381ad15fca76390e11aa77d657b31e593f654be6
Eva <evaplas1999@gmail.com>
2022-06-07 16:03:06 +0200

    added arguments to the load function, so different files can be used from main

--- code/load.py
+++ code/load.py
@@ -1,17 +1,17 @@
 from stations import *
 import csv
 
-def load():
+def load(file_locations: str, file_connections: str):
     """Load the stations and its connections"""
     stationdictionary = {}
 
-    with open("../data/StationsHolland.csv", newline = '') as csvfile:
+    with open(file_locations, newline = '') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader: 
             new_station = Station(row['station'], row['x'], row['y'])
             stationdictionary[row['station']] = new_station
 
-    with open("../data/ConnectiesHolland.csv", newline = '') as csvfile:
+    with open(file_connections, newline = '') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader: 
             connection = Connection(stationdictionary[row['station1']], stationdictionary[row['station2']], row['distance'])
--- main.py
+++ main.py
@@ -5,14 +5,10 @@ import os, sys
 directory = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(directory, 'code'))
 
-# import the used programs
-import load
-
-def main():
-    """Use one of the algorithms to find the best trainroutes."""
-    file_stations = './data/StationsHolland.csv'
-    file_locations = './data/ConnectiesHolland.csv'
-    load()
+from load import *
 
 if __name__ == '__main__':
-    main()
+    file_stations = './data/StationsHolland.csv'
+    file_connections = './data/ConnectiesHolland.csv'
+    stations = load(file_stations, file_connections)
+    print_stationdictionary(stations)
