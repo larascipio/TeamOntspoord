7e87c96ef8fd5c3e854d59283458ab70720b23ed
Karelkaas <timvkok@gmail.com>
2022-06-07 14:01:58 +0200

    added CSV reader

--- tim_klad.py
+++ tim_klad.py
@@ -1,13 +1,27 @@
 import pandas as pd
+import csv
 
 def main():
     # GEBRUIK DICTREADER, LOADER.PY
+    # COMMIT PULL PUSH
 
     # Create the Data Frame from the csv file, and sort by year and rating
     connection_dataFrame = pd.read_csv("data/ConnectiesHolland.csv")
     stations_dataFrame = pd.read_csv("data/StationsHolland.csv")
     print(connection_dataFrame)
     print(stations_dataFrame)
+
+    with open("data/StationsHolland.csv", newline = '') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader: 
+            print(row['station'], (row['x'], row['y']))
+
+    with open("data/ConnectiesHolland.csv", newline = '') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader: 
+            print(row['station1'], (row['station2'], row['distance'])) 
+
+
 
 if __name__ == "__main__":
     main()
