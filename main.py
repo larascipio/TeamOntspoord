4bb94303bfc3d2a4cecf7b17ea51f37107ded7d6
Eva <evaplas1999@gmail.com>
2022-06-07 15:45:38 +0200

    added the files to main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 """
 main.py
 """
+import os, sys
+directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(directory, 'code'))
+
+# import the used programs
+import load
 
 def main():
-    pass
+    """Use one of the algorithms to find the best trainroutes."""
+    file_stations = './data/StationsHolland.csv'
+    file_locations = './data/ConnectiesHolland.csv'
+    load()
 
 if __name__ == '__main__':
     main()
