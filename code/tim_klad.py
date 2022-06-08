9353e050b414abceaca832aa13a984c0c65d934c
Eva <evaplas1999@gmail.com>
2022-06-08 14:49:33 +0200

    added plotly animations and changed the bad algorithm for the national map

--- code/tim_klad.py
+++ code/tim_klad.py
@@ -3,20 +3,20 @@ import matplotlib.pyplot as plt
 
 def visualize_tracks():
     """Visualizes all the tracks in a plot"""
-    file_stations = '../data/StationsHolland.csv'
-    file_connections = '../data/ConnectiesHolland.csv'
+    file_stations = '../data/StationsNationaal.csv'
+    file_connections = '../data/ConnectiesNationaal.csv'
     load(file_stations, file_connections)
     actualagents = list(stationdictionary.values())
 
     plt.figure(figsize=(12, 25))
 
     for station in actualagents:
-        plt.plot(float(station._y), float(station._x), 'o')
-        plt.annotate(station._name, (float(station._y), float(station._x)))
+        plt.plot(float(station._x), float(station._y), 'o')
+        plt.annotate(station._name, (float(station._x), float(station._y)))
 
     for connection in connectionlist:
-        begin = [float(connection._stations[0]._y), float(connection._stations[1]._y)]
-        end = [float(connection._stations[0]._x), float(connection._stations[1]._x)]
+        begin = [float(connection._stations[0]._x), float(connection._stations[1]._x)]
+        end = [float(connection._stations[0]._y), float(connection._stations[1]._y)]
         plt.plot(begin, end, color='g')
 
     plt.title('Stations')
