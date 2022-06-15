b1a15bca31231d53b8af8e6c125d35e447db4d45
Lara Scipio <lara@scipio.nl>
2022-06-15 11:26:22 +0200

    random algorithm

--- code/algorithms/random_algorithm.py
+++ code/algorithms/random_algorithm.py
@@ -1,33 +1,118 @@
 import random
 
-class Train():
-    def __init__(self, starting_station, connections, max_distance):
+class Make_Random_Routes():
+    def __init__(self, railnet, num_trains: int, max_distance: int):
         """
         Create a train at the given station.
         """
-        self._distance = 0
-        self._current_station = starting_station
-        self._total_connections = connections
-        self._route = [self._current_station._name] 
-        self._stations_traveled = [self._current_station]
-        # self._current_station.travel()
-        self.running = True
+        self._railnet = railnet
+        self._max_trains = num_trains
         self._max_dist = max_distance
+        self._tot_stations = len(self._railnet.get_stations())
+        self._tot_connections = len(self._railnet.get_connections())
+        self._trains = []
+    
+    def run(self):
+        # check whether not all connections have been passed 
+        while len(self._railnet.get_passed_connections()) < self._tot_connections:
+            
+            # check if there can be another train
+            if len(self._trains) == self._max_trains:
+                raise Exception('Too many trains made.')
+            
+            # create a train
+            train = self.create_train()
+            if not train:
+                return
 
+            # keep going until the route is 2 hours
+            while train.is_running():
+                # connection = train.choose_connection()
+                connection = train.choose_random_connection()
+
+                if not connection:
+                    break
+
+                if connection.get_distance() + train.get_distance() < self._max_dist:
+                    train.move(connection)
+                else:
+                    train.stop()
+    
+    def create_train(self):
+        start = None
+        # choose random starting point
+        while not start:
+            # choose a random station 
+            possible_stations = []
+            for station in self._railnet.get_stations().values():
+                possible_stations.append(station)
+            start = random.choice(possible_stations)
+
+        return Train(self, start, self._max_dist)
+    
+    def get_trains(self):
+        return self._trains
+    
     def choose_random_connection(self):
         """
         Choose a random connection.
         """
         possible_connections = []
-        for connection in self._current_station._connections:
-            if self._distance + connection._distance <= self._max_dist:
+        for connection in self._current_station.get_connections():
+            if not connection.passed():
                 possible_connections.append(connection)
         if possible_connections:
-            random_connection =  random.choice(possible_connections)
-            self._total_connections.remove(random_connection)
-        self.running = False
+            return random.choice(list(possible_connections))
+
+        # no more possible connections
+        self.stop()
         return None
     
+    def quality(self) -> float:
+        """
+        Calculate the quality of the current routes.
+        """
+        qual = (len(self._railnet.get_passed_connections())/self._tot_connections)*10000
+        for train in self._trains:
+            qual -= 100
+            qual -= train.get_distance()
+        return qual
+
+    def __repr__(self):
+        representation = 'Route:\n'
+        for train in self._trains:
+            representation += f'{train}' + '\n'
+        representation += f'quality = {self.quality()}'
+        return representation
+    def move(self, connection):
+        """
+        Move to a next station.
+        """
+        # print(self._current_station)
+        # increase the distance of the route
+        self._distance += connection.get_distance()
+
+        # move the current station
+        self._current_station = connection.get_destination(self._current_station)
+
+        # add the station and connection to the route
+        self._route.append(self._current_station.get_name())
+        self._stations_traveled.append(self._current_station)
+        self._connections_traveled.append(connection)
+        connection.travel()
+        # print(self.get_connections())
+        # print(self._distance)
+        # print()
+    
+    def get_distance(self):
+        return self._distance
+    
+    def get_route(self):
+        return self._route
+
+    def __repr__(self):
+        return f'Train: {[station for station in self._stations_traveled]}'
+
     # def move(self, connection):
     #     """
     #     Move to a next station.
@@ -54,7 +139,7 @@ class Train():
     #     return self._route
 
 
-def make_random_routes(stations: list, connections: list, max_trains:int, max_distance: int):
+def Make_Random_Routes(stations: list, connections: list, max_trains:int, max_distance: int):
     """
     Use the given railroads to create connections that satisfy the contraints.
     """
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ main.py
 """
 # from code.classes.load import load
 from code.algorithms.bad_algorithm import Make_Bad_Routes
-from code.algorithms.random_algorithm import make_random_routes
+from code.algorithms.random_algorithm import Make_Random_Routes
 from code.visualisation.plotly_animation import create_animation
 from code.classes.structure import Railnet
 from code.visualisation.quality_hist import quality_hist
