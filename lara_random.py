f7dbafe14558fa1334c98207328ac8d452538651
Eva <evaplas1999@gmail.com>
2022-06-16 12:10:11 +0200

    changed quality in output

--- lara_random.py
+++ lara_random.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     rails = Railnet()
     rails.load(file_stations, file_connections)
 
-    for _ in range(1):
+    for _ in range(100):
         route = Make_Random_Routes(rails, max_trains, max_time)
         route.run()
         quality = route.quality()
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 
     print(route)
 
-    output(route.quality(),route.get_trains(),outputfile='code/output/output.csv')
+    output(quality,route.get_trains(),outputfile='code/output/output.csv')
 
     # create_animation(rails, route)
 
